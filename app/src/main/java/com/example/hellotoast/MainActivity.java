cf82dea55437f39cb45ba9dbe15334158a84c740
RyanYee <yeezs-wm19@student.tarc.edu.my>
2020-07-01 15:46:12 +0800

    #update

--- app/src/main/java/com/example/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -2,21 +2,31 @@ package com.example.hellotoast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Button;
+
+
+import com.example.hellotoast.R;
 
 public class MainActivity extends AppCompatActivity {
 
     private int mCount = 0;
     private TextView mShowCount;
+    private Button zero;
+    private Button count;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.show_count);
+        zero = findViewById(R.id.button_zero);
+        count = findViewById(R.id.button_count);
     }
 
     public void showToast(View view) {
@@ -28,5 +38,17 @@ public class MainActivity extends AppCompatActivity {
        ++mCount;
         if (mShowCount != null)
             mShowCount.setText(Integer.toString(mCount));
+        zero.setBackgroundColor(Color.MAGENTA);
+        if(mCount%2==0)
+            count.setBackgroundColor(Color.GREEN);
+        else
+            count.setBackgroundColor(Color.YELLOW);
+    }
+
+    public void callMethod(View view) {
+        mCount = 0;
+        mShowCount.setText(Integer.toString(mCount));
+        zero.setBackgroundColor(0xffaaaaaa);
+        count.setBackgroundColor(Color.BLUE);
     }
 }
